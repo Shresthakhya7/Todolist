fe61684ed1aa7a33c7e57a859b39a61afb0a53b6
shresthakhya <shresthakhya7@gmail.com>
2024-05-20 13:18:26 +0545

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ToDoList from "./ToDoList.js"
+import DigitalClock from "./DigitalClock.js"
+
+function App() {
+
+  return(<>
+  
+  <DigitalClock/>
+  <ToDoList/> </>)
+
+}
+export default App
